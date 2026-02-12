dcef0915c74113e3240f0dfd78be68290f00f5d6
Dmitry <shilov.d.a@gmail.com>
2026-02-12 15:38:14 +0200

    Update rss.py
    Удалена поддержка сезонов

--- rss.py
+++ rss.py
@@ -6,11 +6,17 @@ import json
 import logging
 from logging.handlers import RotatingFileHandler
 from yaml import load as yaml_load
+from yaml import SafeLoader as yaml_loader
 from xml.etree import ElementTree
+import yaml
+
+# var_dump analogue
+def var_dump(var):
+    print(f"{var=}, type={type(var)}")
 
 # Парсинг настроек
 with open(Path(__file__).resolve().parent / 'config.yml', 'r') as yaml_config:
-    config = yaml_load(yaml_config)
+    config = yaml_load(yaml_config, Loader=yaml_loader)
 
 # настройка логирования
 logging.basicConfig(
@@ -27,7 +33,8 @@ logging.basicConfig(
 
 
 # Cookie для авторизации на трекере
-cookies = ';'.join(['{}={}'.format(cookie, config['auth'][cookie]) for cookie in config['auth']])
+cookies = ';'.join(['{}={}'.format(cookie, config['auth'][cookie])
+                   for cookie in config['auth']])
 
 # Строка подключения к transmission RPC
 transmission_url = 'http://{host}:{port}/transmission/rpc/'.format(
@@ -46,7 +53,8 @@ def transmission_rpc_request(rpc_data: dict) -> dict:
             transmission_url,
             data=json.dumps(rpc_data),
             headers={'X-Transmission-Session-Id': transmission_session_id},
-            auth=(config['transmission']['user'], config['transmission']['password']),
+            auth=(config['transmission']['user'],
+                  config['transmission']['password']),
             timeout=config['timeout']
         )
         if torrent_request.status_code == 200:
@@ -57,7 +65,8 @@ def transmission_rpc_request(rpc_data: dict) -> dict:
         torrent_session_search = re.search('X-Transmission-Session-Id: .+?(?=<)',
                                            torrent_request.text)
         if torrent_session_search:
-            transmission_session_id = torrent_session_search.group(0).split(':')[1].strip()
+            transmission_session_id = torrent_session_search.group(0).split(':')[
+                1].strip()
     if torrent_request.status_code != 200:
         logging.error('transmission RPC: {}'.format(torrent_request))
         exit(torrent_request.status_code)
@@ -93,16 +102,19 @@ request_available_torrents = transmission_rpc_request(
 
 catalog = dict()
 for job in request_available_torrents['arguments']['torrents']:
-    if 'LostFilm' not in job['name']:
+    if 'LostFilm.TV' not in job['name']:
         continue
     if ' - LostFilm.TV' in job['name']:
         name = ' '.join(job['name'].split(' - LostFilm.TV')[0].split()[:-1])
-        series = 'S{:02d}E99'.format(int(job['name'].split(' - LostFilm.TV')[0].split()[-1]))
+        series = 'S{:02d}E99'.format(
+            int(job['name'].split(' - LostFilm.TV')[0].split()[-1]))
     else:
         data = job['name'].split('.rus.LostFilm.TV.')[0]
+        #var_dump(data)
         series = data.split('.')[-2]
         quality = data.split('.')[-1]
-        name = data.replace(quality, '').replace(series, '').strip('.').replace('.', ' ')
+        name = data.replace(quality, '').replace(
+            series, '').strip('.').replace('.', ' ')
     # Обработка нестандартного именования серий
     if name in config['aliases']:
         name = config['aliases'][name]
@@ -117,7 +129,9 @@ list_request = requests.get(
     config['url'],
     timeout=config['timeout'])
 list_request.encoding = 'utf-8'
-rss_items = ElementTree.fromstring(list_request.text).find('channel').findall('item')
+#print(list_request.text)
+rss_items = ElementTree.fromstring(
+    list_request.text).find('channel').findall('item')
 
 for item in rss_items:
     title = item.find('title').text
@@ -138,48 +152,38 @@ for item in rss_items:
         logging.warning(f"Не получилось найти имя: {title}")
         continue
 
+    # Качаем только нужные серии
     if (
+        real_name in config['subscriptions'] and
         (
-            # Подписка на сезон
-            (
-                series.endswith('E99') and
-                (
-                    (
-                        # Качаем все завершённые сезоны
-                        isinstance(config['subscriptions_season'], str) and
-                        quality == config['subscriptions_season']
-                    ) or
-                    (
-                        # Качаем только нужные сезоны
-                        isinstance(config['subscriptions_season'], dict) and
-                        real_name in config['subscriptions_season'] and
-                        quality == config['subscriptions_season'][real_name]
-                    )
-                )
-            ) or
-            # Подписка на каждую серию отдельно
-            (
-                real_name in config['subscriptions'] and
-                quality == config['subscriptions'][real_name]
-            )
+            real_name not in config['blacklist'] and
+            quality == config['subscriptions'][real_name] and
+            not series.endswith('E99') and
+            not series.endswith('E999')
         ) and not
-        # Ещё не добавляли
+
+        # И если их нет в списке текущих раздач в transmission
         (
             real_name in catalog and
             series in catalog[real_name]
-        ) and
-        real_name not in config['blacklist']
+        )
     ):
         logging.info(f"Добавляем {title}")
-        logging.debug(f'real_name={real_name}, series={series}, quality={quality}')
+        logging.debug(
+            f'real_name={real_name}, series={series}, quality={quality}')
+
+        download_location=''.join(real_name.strip('.').split(':'))
+
         transmission_rpc_request({
             'arguments': {
                 'cookies': cookies,
                 'filename': link,
                 # Имя директории не может оканчиваться точкой
-                'download-dir': str(download_root / real_name.strip('.'))
+                'download-dir': str(download_root / download_location)
             },
             'method': 'torrent-add'
         })
     else:
-        logging.debug(f'Пропуск real_name={real_name}, series={series}, quality={quality}')
+        logging.debug(
+            f'Пропуск real_name={real_name}, series={series}, quality={quality}')
+
